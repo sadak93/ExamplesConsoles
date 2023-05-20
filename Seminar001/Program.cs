730fa74a158f495282f923328e1196e75fca319c
Sadak <s.sadak02@gmail.com>
2023-05-20 19:02:32 +0600

    add Chislo i ego kvadrat

---
+++ Seminar001/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите целое число ");
+int num = Convert.ToInt32 (Console.ReadLine());
+int result = num*num;
+System.Console.WriteLine("Квадрат введенного числа равен "+result);
+
+if (num>0)
+{
+    System.Console.WriteLine("Вы ввели положительное число");
+}
+else if (num==0)
+    System.Console.WriteLine("Вы ввели нуль");
+else 
+    System.Console.WriteLine("Вы ввели отрицательное число");
+    
