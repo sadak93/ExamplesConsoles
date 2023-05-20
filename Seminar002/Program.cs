1119de2fa7f146eff761579e0e061469d863b75b
Sadak <s.sadak02@gmail.com>
2023-05-20 20:36:51 +0600

    add Proverka kvadrata

---
+++ Seminar002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите целое число ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите 2ое целое число ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1==num2*num2)
+{
+    System.Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    System.Console.WriteLine("Первое число не является квадратом второго");
+}
